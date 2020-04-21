8b677b61e3c3a7f4a0a3c3805e0b3aa142c7e6bd
AveryTheKitty <nightmare.nkl@gmail.com>
2020-04-21 03:08:53 -0500

    HIDF vehicles (encoding only)
    Added: HIDF Zamak (variants)
    Added: HIDF Quad Bike
    Added: HIDF MB 4WD (variants)

--- a3_atlas/soft_f_atlas/Quadbike_01/config.cpp
+++ a3_atlas/soft_f_atlas/Quadbike_01/config.cpp
@@ -27,8 +27,22 @@ class CfgVehicles
 					BLU_E_F
 				};
 			};
+			class Jungle
+			{
+				displayName = $STR_A3_TEXTURESOURCES_JUNGLE0;
+				author = $STR_A3_A_AveryTheKitty;
+				textures[] =
+                {
+                    "\A3_Atlas\Soft_F_Atlas\Offroad_02\Data\Offroad_02_ext_jungle_CO.paa",
+                    "\A3_Atlas\Soft_F_Atlas\Offroad_02\Data\Offroad_02_ext_jungle_CO.paa",
+                    "\A3_Atlas\Soft_F_Atlas\Offroad_02\Data\Offroad_02_int_jungle_CO.paa",
+                    "\A3_Atlas\Soft_F_Atlas\Offroad_02\Data\Offroad_02_int_jungle_CO.paa"
+                };
+				factions[] = {BLU_HIDF_F};
+			};
 		};
 	};
 	// Arma 3 Atlas
+	#include "cfgHIDF.hpp"
 	#include "cfgBlufor_Atlas.hpp"
 };
---
+++ a3_atlas/soft_f_atlas/Truck_02/config.cpp
@@ -0,0 +1,16 @@
+#include "\A3_Aegis\basicDefines_A3.hpp"
+#include "cfgPatches.hpp"
+class CfgVehicles
+{
+	// Arma 3
+	class Truck_F;
+	class Truck_02_base_F: Truck_F
+	{
+		class Turrets;
+	};
+	class Truck_02_transport_base_F;
+	// Arma 3 Aegis
+	class Truck_02_cargo_base_F;
+	// Arma 3 Atlas
+	#include "cfgHIDF.hpp"
+};
