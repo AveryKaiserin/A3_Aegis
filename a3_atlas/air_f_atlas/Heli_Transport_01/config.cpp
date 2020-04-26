b762b7cd029e9d90484b6ce411ac6d5fafe9c5c1
AveryTheKitty <nightmare.nkl@gmail.com>
2020-04-26 13:30:49 -0500

    Added IDF UH-80 Ghost Hawk
    Added: IDF UH-80 Ghost Hawk & livery

---
+++ a3_atlas/air_f_atlas/Heli_Transport_01/config.cpp
@@ -0,0 +1,43 @@
+#include "\A3_Aegis\basicDefines_A3.hpp"
+#include "cfgPatches.hpp"
+class CfgVehicles
+{
+	// Arma 3
+	class Helicopter;
+	class Helicopter_Base_F: Helicopter
+	{
+		class Turrets;
+	};
+	class Helicopter_Base_H: Helicopter_Base_F
+	{
+		class Turrets: Turrets
+		{
+			class CopilotTurret;
+			class MainTurret;
+		};
+	};
+	class Heli_Transport_01_base_F: Helicopter_Base_H
+	{
+		class Turrets: Turrets
+		{
+			class MainTurret;
+			class RightDoorGun;
+		};
+		class TextureSources
+		{
+			class IDF
+			{
+				displayName = $STR_A3_A_TEXTURESOURCES_IDF0;
+				author = $STR_A3_A_AveryTheKitty;
+				textures[] =
+				{
+					"\A3_Atlas\Air_F_Atlas\Heli_Transport_01\Data\Heli_Transport_01_ext01_IDF_CO.paa",
+					"\A3_Atlas\Air_F_Atlas\Heli_Transport_01\Data\Heli_Transport_01_ext02_IDF_CO.paa"
+				};
+				factions[] = {IND_I_F};
+			};
+		};
+	};
+	// Arma 3 Atlas
+	#include "cfgIDF.hpp"
+};
