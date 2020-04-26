5ae564f0a3f28b1da0eb2922d83697af925d9d0d
AveryTheKitty <nightmare.nkl@gmail.com>
2020-04-26 13:17:34 -0500

    Added simple object configuration for some drones

--- a3_aegis/air_f_aegis/UAV_01/cfgIon.hpp
+++ a3_aegis/air_f_aegis/UAV_01/cfgIon.hpp
@@ -1,6 +1,47 @@
-class B_ION_UAV_01_F: B_UAV_01_F
+class B_ION_UAV_01_F: UAV_01_base_F
 {
 	author = $STR_A3_A_AveryTheKitty;
+	class SimpleObject
+	{
+		eden = true;
+		animate[] =
+		{
+			{damagehide,0},
+			{rotorimpacthide,0},
+			{tailrotorimpacthide,0},
+			{propeller1_rotation,0},
+			{propeller1_blur_rotation,0},
+			{propeller2_rotation,0},
+			{propeller2_blur_rotation,0},
+			{propeller3_rotation,0},
+			{propeller3_blur_rotation,0},
+			{propeller4_rotation,0},
+			{propeller4_blur_rotation,0},
+			{propeller1_hide,0},
+			{propeller1_blur_hide,0},
+			{propeller2_hide,0},
+			{propeller2_blur_hide,0},
+			{propeller3_hide,0},
+			{propeller3_blur_hide,0},
+			{propeller4_hide,0},
+			{propeller4_blur_hide,0},
+			{mainturret,0},
+			{maingun,-0.05}
+		};
+		hide[] =
+		{
+			zasleh,
+			"tail rotor blur",
+			"main rotor blur",
+			"zadni svetlo",
+			clan,
+			"podsvit pristroju",
+			poskozeni
+		};
+		verticalOffset = 0.15;
+		verticalOffsetWorld = -0.001;
+		init = "''";
+	};
 	editorPreview = "\A3_Aegis\EditorPreviews_F_Aegis\Data\CfgVehicles\B_ION_UAV_01_F.jpg";
 	scope = public;
 	scopeCurator = public;
@@ -9,8 +50,7 @@ class B_ION_UAV_01_F: B_UAV_01_F
 	faction = BLU_ION_F;
 	crew = B_UAV_AI_F;
 	typicalCargo[] = {B_UAV_AI_F};
-	textureList[] = {ION,1};
-	hiddenSelectionsTextures[] = {"\A3_Aegis\Air_F_Aegis\UAV_01\Data\UAV_01_ION_CO.paa"};
+	accuracy = 0.5;
 	class assembleInfo
 	{
 		primary = true;
@@ -19,6 +59,8 @@ class B_ION_UAV_01_F: B_UAV_01_F
 		displayName = "";
 		dissasembleTo[] = {B_ION_UAV_01_backpack_F};
 	};
+	hiddenSelectionsTextures[] = {"\A3_Aegis\Air_F_Aegis\UAV_01\Data\UAV_01_ION_CO.paa"};
+	textureList[] = {ION,1};
 };
 class B_ION_UAV_01_backpack_F: Weapon_Bag_Base
 {
--- a3_aegis/air_f_aegis/UAV_01/cfgSpetsnaz.hpp
+++ a3_aegis/air_f_aegis/UAV_01/cfgSpetsnaz.hpp
@@ -1,6 +1,47 @@
-class O_R_UAV_01_F: O_UAV_01_F
+class O_R_UAV_01_F: UAV_01_base_F
 {
 	author = $STR_A3_A_AveryTheKitty;
+	class SimpleObject
+	{
+		eden = true;
+		animate[] =
+		{
+			{damagehide,0},
+			{rotorimpacthide,0},
+			{tailrotorimpacthide,0},
+			{propeller1_rotation,0},
+			{propeller1_blur_rotation,0},
+			{propeller2_rotation,0},
+			{propeller2_blur_rotation,0},
+			{propeller3_rotation,0},
+			{propeller3_blur_rotation,0},
+			{propeller4_rotation,0},
+			{propeller4_blur_rotation,0},
+			{propeller1_hide,0},
+			{propeller1_blur_hide,0},
+			{propeller2_hide,0},
+			{propeller2_blur_hide,0},
+			{propeller3_hide,0},
+			{propeller3_blur_hide,0},
+			{propeller4_hide,0},
+			{propeller4_blur_hide,0},
+			{mainturret,0},
+			{maingun,-0.05}
+		};
+		hide[] =
+		{
+			zasleh,
+			"tail rotor blur",
+			"main rotor blur",
+			"zadni svetlo",
+			clan,
+			"podsvit pristroju",
+			poskozeni
+		};
+		verticalOffset = 0.15;
+		verticalOffsetWorld = -0.001;
+		init = "''";
+	};
 	editorPreview = "\A3_Aegis\EditorPreviews_F_Aegis\Data\CfgVehicles\O_R_UAV_01_F.jpg";
 	scope = public;
 	scopeCurator = public;
@@ -9,8 +50,7 @@ class O_R_UAV_01_F: O_UAV_01_F
 	faction = OPF_R_F;
 	crew = O_UAV_AI;
 	typicalCargo[] = {O_R_Soldier_Lite_F};
-	textureList[] = {Rus,1};
-	hiddenSelectionsTextures[] = {"\A3_Aegis\Air_F_Aegis\UAV_01\Data\UAV_01_RUS_CO.paa"};
+	accuracy = 0.5;
 	class assembleInfo
 	{
 		primary = true;
@@ -19,6 +59,8 @@ class O_R_UAV_01_F: O_UAV_01_F
 		displayName = "";
 		dissasembleTo[] = {O_R_UAV_01_backpack_F};
 	};
+	hiddenSelectionsTextures[] = {"\A3_Aegis\Air_F_Aegis\UAV_01\Data\UAV_01_RUS_CO.paa"};
+	textureList[] = {Rus,1};
 };
 class O_R_UAV_01_backpack_F: Weapon_Bag_Base
 {
