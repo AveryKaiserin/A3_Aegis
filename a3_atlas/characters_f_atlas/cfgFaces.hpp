beb3aa4496a9d9997ce897ab2ef145733c8b2a80
AveryTheKitty <nightmare.nkl@gmail.com>
2020-04-28 21:58:23 -0500

    Fixed some faces not appearing

--- a3_atlas/characters_f_atlas/cfgFaces.hpp
+++ a3_atlas/characters_f_atlas/cfgFaces.hpp
@@ -8,60 +8,134 @@ class CfgFaces
         class WhiteHead_01;
 		class WhiteHead_05: WhiteHead_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_NATO,
+				Head_Euro,
+				Head_IDF
+			};
 		};
 		class WhiteHead_11: WhiteHead_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_NATO,
+				Head_Euro,
+				Head_IDF
+			};
 		};
 		class WhiteHead_16: WhiteHead_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_NATO,
+				Head_Euro,
+				Head_IDF
+			};
 		};
 		class WhiteHead_18: WhiteHead_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_NATO,
+				Head_Euro,
+				Head_IDF
+			};
 		};
 		class GreekHead_A3_01;
 		class GreekHead_A3_09: Default
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_Greek,
+				Head_NATO,
+				Head_IDF
+			};
+		};
+		class Kerry;
+		class Kerry_C_F: Kerry
+		{
+			identityTypes[] =
+			{
+				Head_NATO,
+				Head_Euro,
+				Head_IDF
+			};
 		};
         // Arma 3 Laws of War
 		class GreekHead_A3_12: GreekHead_A3_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Orange_Civilian,
+				Head_Greek,
+				Head_IDF
+			};
 		};
 		class GreekHead_A3_14: GreekHead_A3_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Orange_AAF,
+				Head_Greek,
+				Head_IDF
+			};
 		};
         // Arma 3 Tacops
 		class Mavros: GreekHead_A3_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_Greek,
+				Head_IDF
+			};
 		};
         // Arma 3 Enoch
 		class LivonianHead_7: WhiteHead_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_Enoch,
+				Head_NATO,
+				Head_IDF
+			};
 		};
 		class LivonianHead_8: WhiteHead_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_Enoch,
+				Head_NATO,
+				Head_IDF
+			};
 		};
 		class LivonianHead_9: WhiteHead_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_Enoch,
+				Head_NATO,
+				Head_IDF
+			};
 		};
 		class LivonianHead_10: WhiteHead_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_Enoch,
+				Head_NATO,
+				Head_IDF
+			};
 		};
         // Arma 3 Aegis
 		class WhiteHead_33: WhiteHead_01
 		{
-			identityTypes[] += {Head_IDF};
+			identityTypes[] =
+			{
+				Head_NATO,
+				Head_Euro,
+				Head_IDF
+			};
 		};
     };
 };
