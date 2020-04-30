95abffc609daca9f9e43e3d2e16272d1886a4e34
AveryTheKitty <nightmare.nkl@gmail.com>
2020-04-29 22:47:20 -0500

    Update cfgAmmo.hpp

--- a3_atlas/weapons_f_atlas/cfgAmmo.hpp
+++ a3_atlas/weapons_f_atlas/cfgAmmo.hpp
@@ -2,11 +2,20 @@ class CfgAmmo
 {
 	// Arma 3
 	class BulletBase;
+	class B_20mm;
 	class B_25mm;
 	class I_IRStrobe;
 	// Arma 3 Marksman
     class B_93x64_Ball;
 	// Arma 3 Atlas
+	class B_20mm_Tracer_Yellow: B_20mm
+	{
+		model = "\A3\Weapons_F\Data\bullettracer\tracer_yellow.p3d";
+	};
+	class B_25mm_Tracer_Yellow: B_25mm
+	{
+		model = "\A3\Weapons_F\Data\bullettracer\tracer_yellow.p3d";
+	};
 	class B_25mm_Tracer_Yellow: B_25mm
 	{
 		model = "\A3\Weapons_F\Data\bullettracer\tracer_yellow.p3d";
