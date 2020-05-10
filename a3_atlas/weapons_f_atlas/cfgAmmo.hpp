8aa5f1d1f12919f1e2ba307a44a2c244c7cac5ec
AveryTheKitty <nightmare.nkl@gmail.com>
2020-05-10 12:22:16 -0500

    Update cfgAmmo.hpp

--- a3_atlas/weapons_f_atlas/cfgAmmo.hpp
+++ a3_atlas/weapons_f_atlas/cfgAmmo.hpp
@@ -16,10 +16,6 @@ class CfgAmmo
 	{
 		model = "\A3\Weapons_F\Data\bullettracer\tracer_yellow.p3d";
 	};
-	class B_25mm_Tracer_Yellow: B_25mm
-	{
-		model = "\A3\Weapons_F\Data\bullettracer\tracer_yellow.p3d";
-	};
 	class I_I_IRStrobe: I_IRStrobe{};
     class B_93x64_Ball_Tracer_Red: B_93x64_Ball
 	{
