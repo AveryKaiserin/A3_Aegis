f3a48691a5203ca7d5f8e5c05f8fa210e9c58fef
AveryTheKitty <nightmare.nkl@gmail.com>
2020-06-10 20:07:34 -0500

    Update cfgUnitInsignia.hpp

--- a3_atlas/ui_f_atlas/cfgUnitInsignia.hpp
+++ a3_atlas/ui_f_atlas/cfgUnitInsignia.hpp
@@ -17,35 +17,28 @@ class CfgUnitInsignia
 	class HIDF_Placeholder
 	{
 		displayName = "HIDF placeholder insignia";
-		texture = "\A3_Atlas\UI_F_Atlas\Data\CfgUnitInsignia\HIDF_Placeholder_CA.paa";
+		//texture = "\A3_Atlas\UI_F_Atlas\Data\CfgUnitInsignia\HIDF_Placeholder_CA.paa";
 		author = $STR_A3_A_AveryTheKitty;
 		material = "\A3\UI_F\Data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 	};
 	class IDF_Placeholder
 	{
 		displayName = "HIDF placeholder insignia";
-		texture = "\A3_Atlas\UI_F_Atlas\Data\CfgUnitInsignia\IDF_Placeholder_CA.paa";
+		//texture = "\A3_Atlas\UI_F_Atlas\Data\CfgUnitInsignia\IDF_Placeholder_CA.paa";
 		author = $STR_A3_A_AveryTheKitty;
 		material = "\A3\UI_F\Data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 	};
 	class CDF_Placeholder
 	{
 		displayName = "CDF placeholder insignia";
-		texture = "\A3_Atlas\UI_F_Atlas\Data\CfgUnitInsignia\CDF_Placeholder_CA.paa";
-		author = $STR_A3_A_AveryTheKitty;
-		material = "\A3\UI_F\Data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
-	};
-	class EA_Placeholder
-	{
-		displayName = "EA placeholder insignia";
-		texture = "\A3_Atlas\UI_F_Atlas\Data\CfgUnitInsignia\EA_Placeholder_CA.paa";
+		//texture = "\A3_Atlas\UI_F_Atlas\Data\CfgUnitInsignia\CDF_Placeholder_CA.paa";
 		author = $STR_A3_A_AveryTheKitty;
 		material = "\A3\UI_F\Data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 	};
 	class ADF_Placeholder
 	{
 		displayName = "ADF placeholder insignia";
-		texture = "\A3_Atlas\UI_F_Atlas\Data\CfgUnitInsignia\ADF_Placeholder_CA.paa";
+		//texture = "\A3_Atlas\UI_F_Atlas\Data\CfgUnitInsignia\ADF_Placeholder_CA.paa";
 		author = $STR_A3_A_AveryTheKitty;
 		material = "\A3\UI_F\Data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 	};
