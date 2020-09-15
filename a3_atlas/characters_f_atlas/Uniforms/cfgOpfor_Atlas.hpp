b7724715329a998403170c0b64b6b8ae75768fc9
AveryTheKitty <nightmare.nkl@gmail.com>
2020-09-15 11:59:44 -0500

    e

--- a3_atlas/characters_f_atlas/Uniforms/cfgOpfor_Atlas.hpp
+++ a3_atlas/characters_f_atlas/Uniforms/cfgOpfor_Atlas.hpp
@@ -19,7 +19,7 @@ class U_O_officer_noInsignia_semiarid_F: Uniform_Base
 {
 	author = $STR_A3_A_AveryTheKitty;
 	scope = public;
-	displayName = $STR_A3_A_CfgWeapons_U_O_officer_noInsignia_semiarid0;
+	displayName = $STR_A3_A_CfgWeapons_U_O_officer_noInsignia_semiarid_F0;
 	//picture = "\A3_Atlas\Characters_F_Atlas\Uniforms\Data\UI\icon_U_O_officer_noInsignia_semiarid_CA.paa";
 	model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
 	hiddenSelections[] = {camo};
