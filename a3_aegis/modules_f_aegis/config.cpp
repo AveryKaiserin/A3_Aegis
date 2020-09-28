23587123f6641f577731d2c0567d4c24254c1f88
AveryTheKitty <nightmare.nkl@gmail.com>
2020-09-27 23:28:09 -0500

    d

--- a3_aegis/modules_f_aegis/config.cpp
+++ a3_aegis/modules_f_aegis/config.cpp
@@ -21,23 +21,23 @@ class CfgVehicles
 				{
 					class Dogs
 					{
-						name = $STR_A3_A_CfgVehicles_ModuleAnimalsDog_F;
+						name = $STR_A3_A_CfgVehicles_Aegis_ModuleAnimalsDog_F;
 						value = Fin_random_F;
 					};
 					class Crows
 					{
-						name = $STR_A3_A_CfgVehicles_ModuleAnimalsCrow_F;
+						name = $STR_A3_A_CfgVehicles_Aegis_ModuleAnimalsCrow_F;
 						value = Crowe;
 					};
 					class Eagle
 					{
-						name = $STR_A3_A_CfgVehicles_ModuleAnimalsEagle_F;
+						name = $STR_A3_A_CfgVehicles_Aegis_ModuleAnimalsEagle_F;
 						value = Eagle_F;
 					};
 					class Cows
 					{
-						name = $STR_A3_A_CfgVehicles_ModuleAnimalsCow_F;
-						value = Cow_random_F;
+						name = $STR_A3_A_CfgVehicles_Aegis_ModuleAnimalsCow_F;
+						value = Aegis_Cow_random_F;
 					};
 				};
 			};
@@ -68,44 +68,44 @@ class CfgVehicles
 		};
 	};
 	// Arma 3 Aegis
-	class ModuleAnimalsDog_F: ModuleAnimals_F
+	class Aegis_ModuleAnimalsDog_F: ModuleAnimals_F
 	{
 		author = $STR_A3_A_AveryTheKitty;
 		scope = protected;
 		scopeCurator = public;
-		displayName = $STR_A3_A_CfgVehicles_ModuleAnimalsDog_F;
+		displayName = $STR_A3_A_CfgVehicles_Aegis_ModuleAnimalsDog_F;
 		category = Animals;
 		portrait = "\A3_Aegis\Modules_F_Aegis\Data\portraitAnimalsDogs_CA.paa";
 		animalType = Fin_random_F;
 	};
-	class ModuleAnimalsCrow_F: ModuleAnimals_F
+	class Aegis_ModuleAnimalsCrow_F: ModuleAnimals_F
 	{
 		author = $STR_A3_A_AveryTheKitty;
 		scope = protected;
 		scopeCurator = public;
-		displayName = $STR_A3_A_CfgVehicles_ModuleAnimalsCrow_F;
+		displayName = $STR_A3_A_CfgVehicles_Aegis_ModuleAnimalsCrow_F;
 		category = Animals;
 		portrait = "\A3_Aegis\Modules_F_Aegis\Data\portraitAnimalsCrows_CA.paa";
 		animalType = Crowe;
 	};
-	class ModuleAnimalsEagle_F: ModuleAnimals_F
+	class Aegis_ModuleAnimalsEagle_F: ModuleAnimals_F
 	{
 		author = $STR_A3_A_AveryTheKitty;
 		scope = protected;
 		scopeCurator = public;
-		displayName = $STR_A3_A_CfgVehicles_ModuleAnimalsEagle_F;
+		displayName = $STR_A3_A_CfgVehicles_Aegis_ModuleAnimalsEagle_F;
 		category = Animals;
 		portrait = "\A3_Aegis\Modules_F_Aegis\Data\portraitAnimalsEagles_CA.paa";
 		animalType = Eagle_F;
 	};
-	class ModuleAnimalsCow_F: ModuleAnimals_F
+	class Aegis_ModuleAnimalsCow_F: ModuleAnimals_F
 	{
 		author = $STR_A3_A_AveryTheKitty;
 		scope = protected;
 		scopeCurator = public;
-		displayName = $STR_A3_A_CfgVehicles_ModuleAnimalsCow_F;
+		displayName = $STR_A3_A_CfgVehicles_Aegis_ModuleAnimalsCow_F;
 		category = Animals;
 		portrait = "\A3_Aegis\Modules_F_Aegis\Data\portraitAnimalsCows_CA.paa";
-		animalType = Cow_random_F;
+		animalType = Aegis_Cow_random_F;
 	};
 };
