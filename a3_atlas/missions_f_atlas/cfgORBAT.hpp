da8e7e8fbe346e2ae0b661b4b0c4e88684d51bb6
Senicluxus <senicluxus@gmail.com>
2020-09-30 17:49:20 -0400

    Added ORBAT to Atlas

--- a3_atlas/missions_f_atlas/cfgORBAT.hpp
+++ a3_atlas/missions_f_atlas/cfgORBAT.hpp
@@ -1,4 +1,498 @@
 class CfgORBAT
 {
+    // 
+    // German 31st Panzergrenadier Battalion
+    //
 
+    class 31stbattalion
+	{
+		id = 31;
+		idType = 0;
+		type = "MechanizedInfantry";
+		side = "West";
+		size = "Battalion"
+        insignia = __EVAL((__FILE__ select [0, count __FILE__ - 15])+"img\NATO_31stBattalion_CA");
+		colorInsignia[] = {0,0,0,1};
+		commander = "Rehkopf";
+		commanderRank = "Major";
+		description = "The 31st Panzergrenadier Battalion is Germany's primary expeditionary force, and among the best equipped units in the German military, surviving military expenditure cuts in the 2020's amidst a faltering European economy.";
+		text = "31st Panzergrenadier Bn.";
+		textShort = "%1 %3";
+		
+		class 31stHQ
+		{
+			id = 1;
+			idType = 2;
+			type = "HQ";
+			side = "West";
+			size = "Company";
+			commander = "Rehkopf";
+			commanderRank = "Major";
+			text = "Headquarters Element";
+			textShort = "HQ";
+		};
+
+		class 31stArmoredCompany1
+		{
+			id = 1;
+			idType = 2;
+			type = "Armored";
+			side = "West";
+			size = "Company";
+			commander = "GermanMen";
+			commanderRank = "Captain";
+			text = "%1 %3";
+			textShort = "%1 %3";
+			assets[] = {{B_E_MBT_03_cannon_f,14},{B_E_LT_01_cannon_f,2},{B_E_LT_01_AT_f,2},{B_E_LT_01_AA_f,3},{B_E_LT_01_scout_f,1}};
+        
+			class 31stArmoredPlatoon1
+			{
+				id = 1;
+				idType = 0;
+				type = "Recon";
+				side = "West";
+				commander = "GermanMen";
+				commanderRank = "Lieutenant";
+				size = "Platoon";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{B_E_LT_01_cannon_f,2},{B_E_LT_01_AT_f,2}};
+			};
+			class 31stArmoredPlatoon2
+			{
+				id = 2;
+				idType = 0;
+				type = "Armored";
+				side = "West";
+				commander = "GermanMen";
+				commanderRank = "Lieutenant";
+				size = "Platoon";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{B_E_MBT_03_cannon_f,4}};
+			};
+			class 31stArmoredPlatoon3
+			{
+				id = 3;
+				idType = 0;
+				type = "Armored";
+				commander = "GermanMen";
+				commanderRank = "Lieutenant";
+				side = "West";
+				size = "Platoon";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{B_E_MBT_03_cannon_f,4}};
+			};
+			class 31stArmoredPlatoon4
+			{
+				id = 4;
+				idType = 0;
+				type = "Armored";
+				side = "West";
+				commander = "GermanMen";
+				commanderRank = "Lieutenant";
+				size = "Platoon";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{B_E_MBT_03_cannon_f,4}};
+			};
+			class 31stArmoredPlatoon5
+			{
+				id = 5;
+				idType = 0;
+				type = "Antiair";
+				side = "West";
+				commander = "GermanMen";
+				commanderRank = "Lieutenant";
+				size = "Platoon";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{B_E_LT_01_AA_f,3},{B_E_LT_01_scout_f,1}};
+			};
+		};
+		
+		class 31stArmoredCompany2
+		{
+			id = 2;
+			idType = 2;
+			type = "Armored";
+			side = "West";
+			size = "Company";
+			commander = "GermanMen";
+			commanderRank = "Captain";
+			text = "%1 %3";
+			textShort = "%1 %3";
+			assets[] = {{B_E_MBT_03_cannon_f,14},{B_E_LT_01_cannon_f,2},{B_E_LT_01_AT_f,2},{B_E_LT_01_AA_f,3},{B_E_LT_01_scout_f,1}};
+		}
+
+		class 31stMechanizedInfantryCompany1
+		{
+			id = 3;
+			idType = 2;
+			side = "West";
+			size = "Company";
+			type = "MechanizedInfantry";
+			commander = "GermanMen";
+			commanderRank = "Captain";
+			text = "%1 %3";
+			textShort = "%1 %3";
+			assets[] = {{B_E_APC_Wheeled_03_cannon_F,16}};
+		};
+
+		class 31stMechanizedInfantryCompany2
+		{
+			id = 4;
+			idType = 2;
+			side = "West";
+			size = "Company";
+			type = "MechanizedInfantry";
+			commander = "GermanMen";
+			commanderRank = "Captain";
+			text = "%1 %3";
+			textShort = "%1 %3";
+			assets[] = {{B_E_APC_Wheeled_03_cannon_F,16}};
+		};
+		
+		class 31stSupportCompany
+		{
+			id = 5;
+			idType = 2;
+			side = "West";
+			size = "Company";
+			type = "Support";
+			commander = "GermanMen";
+			commanderRank = "Captain";
+			text = "%2 %3";
+			textShort = "Support";
+		};
+	};
+
+    //
+    // Belarussian 7th Bear Regiment
+    //
+
+    class bearregiment
+	{
+		id = 7;
+		idType = 0;
+		type = "MechanizedInfantry";
+		side = "East";
+		size = "Regiment"
+		insignia = __EVAL((__FILE__ select [0, count __FILE__ - 15])+"img\Bear_Regiment_Icon");
+		colorInsignia[] = {0,0,0,1};
+		commander = "Kamienski";
+		commanderRank = "Colonel";
+		text = "%1 %2 %3";
+		textShort = "%1 %3";
+		description = "The 7th Mechanized Infantry Regiment, nicknamed Bear Regiment, is a mainly mechanized force, using a mix of old Russian equipment and new CSAT arms deliveries.";
+		
+		class 7thFirstBn
+		{
+			id = 1;
+			idType = 0;
+			type = "Infantry";
+			side = "East";
+			size = "Battalion";
+			commander = "Adamovič";
+			commanderRank = "Major";
+			text = "1st Infantry Battalion";
+			textShort = "%1 %3";
+			
+			class FirstBnHQ
+			{
+				id = 1;
+				idType = 0;
+				type = "HQ";
+				side = "East";
+				size = "Company";
+				commander = "Adamovič";
+				commanderRank = "Major";
+				text = "%2 %3";
+				textShort = "%2";
+			};
+			class FirstBnCompany1
+			{
+				id = 1;
+				idType = 0;
+				type = "Infantry";
+				side = "East";
+				size = "Company";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{O_W_APC_Wheeled_02_rcws_v2_ghex_f,16}};
+			};
+			class FirstBnCompany2
+			{
+				id = 2;
+				idType = 0;
+				type = "Infantry";
+				side = "East";
+				size = "Company";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{O_W_APC_Wheeled_02_rcws_v2_ghex_f,16}};
+			};
+			class FirstBnCompany3
+			{
+				id = 3;
+				idType = 0;
+				type = "Infantry";
+				side = "East";
+				size = "Company";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{O_W_APC_Wheeled_02_rcws_v2_ghex_f,16}};
+			};
+			class FirstBnCompany4
+			{
+				id = 4;
+				idType = 0;
+				type = "MechanizedInfantry";
+				side = "East";
+				size = "Company";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{O_W_APC_Tracked_02_cannon_ghex_f,16}};
+			};
+		};
+
+		class 7thSecondBn
+		{
+			id = 2;
+			idType = 0;
+			type = "MechanizedInfantry";
+			side = "East";
+			size = "Battalion";
+			commander = "Sviontak";
+			commanderRank = "Major";
+			text = "2nd Mechanized Infantry Battalion";
+			textShort = "%1 %3";
+			
+			class SecondBnHQ
+			{
+				id = 1;
+				idType = 0;
+				type = "HQ";
+				side = "East";
+				size = "Company";
+				commander = "Sviontak";
+				commanderRank = "Major";
+				text = "%2 %3";
+				textShort = "%2";
+			};
+			class SecondBnCompany1
+			{
+				id = 1;
+				idType = 0;
+				type = "Armored";
+				side = "East";
+				size = "Company";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{O_W_MBT_02_cannon_ghex_f,14}};
+			};
+			class SecondBnCompany2
+			{
+				id = 2;
+				idType = 0;
+				type = "MechanizedInfantry";
+				side = "East";
+				size = "Company";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{O_W_APC_Tracked_02_cannon_ghex_f,16}};
+			};
+			class SecondBnCompany3
+			{
+				id = 3;
+				idType = 0;
+				type = "MechanizedInfantry";
+				side = "East";
+				size = "Company";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{O_W_APC_Tracked_02_cannon_ghex_f,16}};
+			};
+			class SecondBnCompany4
+			{
+				id = 4;
+				idType = 0;
+				type = "Infantry";
+				side = "East";
+				size = "Company";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 %2 %3";
+				textShort = "%1 %3";
+				assets[] = {{O_W_APC_Wheeled_02_rcws_v2_ghex_f,16}};
+			};
+		};
+
+		class 7thThirdBn
+		{
+			id = 3;
+			idType = 0;
+			side = "East";
+			size = "Battalion";
+			type = "Armored";
+			commander = "Rasolka";
+			commanderRank = "Major";
+			text = "3rd Armor Battalion";
+			textShort = "%1 %3";
+		};
+
+		class 7thFourthBn
+		{
+			id = 4;
+			idType = 0;
+			side = "East";
+			size = "Squadron";
+			type = "Cavalry";
+			commander = "Skaryna";
+			commanderRank = "Major";
+			text = "4th Reconnaisance Squadron";
+			textShort = "%1 %3";
+		};
+		
+		class 7thFifthBn
+		{
+			id = 5;
+			idType = 0;
+			side = "East";
+			size = "Battalion";
+			type = "Helicopter";
+			commander = "Krupin";
+			commanderRank = "Major";
+			text = "5th Air Assault Battalion";
+			textShort = "%1 %3";
+			
+			class FifthBnSquadron1
+			{
+				id = 1;
+				idType = 0;
+				side = "East";
+				size = "Company";
+				type = "Helicopter";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 Air Assault %3";
+				textShort = "Air Assault";
+				assets[] = {{O_W_Heli_Attack_02_dynamicLoadout_f,9}};
+			};
+			class FifthBnSquadron2
+			{
+				id = 2;
+				idType = 0;
+				side = "East";
+				size = "Company";
+				type = "Helicopter";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 Air Cavalry %3";
+				textShort = "Air Cavalry";
+				assets[] = {{O_W_Heli_Light_02_dynamicLoadout_f,9}};
+			};
+			class FifthBnSquadron3
+			{
+				id = 3;
+				idType = 0;
+				side = "East";
+				size = "Company";
+				type = "Helicopter";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 Air Cavalry %3";
+				textShort = "Air Cavalry";
+				assets[] = {{O_W_Heli_Light_02_dynamicLoadout_f,9}};
+			};
+			class FifthBnSquadron4
+			{
+				id = 4;
+				idType = 0;
+				side = "East";
+				size = "Company";
+				type = "Helicopter";
+				commander = "RussianMen";
+				commanderRank = "Captain";
+				text = "%1 Air Utility %3";
+				textShort = "Air Utility";
+				assets[] = {{O_T_Heli_Transport_04_f,9}};
+			};
+		};
+		class 7thSupportBn
+		{
+			id = 6;
+			idType = 0;
+			side = "East";
+			size = "Battalion";
+			type = "Support";
+			commander = "Petrenko";
+			commanderRank = "Major";
+			text = "6th Support Battalion";
+			textShort = "%1 %3";
+				class brsupport
+				{
+					id = 7;
+					idType = 0;
+					side = "East";
+					size = "Company";
+					type = "Support";
+					commander = "RussianMen";
+					commanderRank = "Captain";
+					text = "%2 %3";
+					textShort = "%2";
+				};
+				class brmedical
+				{
+					id = 7;
+					idType = 0;
+					side = "East";
+					size = "Company";
+					type = "Medical";
+					commander = "RussianMen";
+					commanderRank = "Captain";
+					text = "%2 %3";
+					textShort = "%2";
+				};
+				class brmaintenance
+				{
+					id = 7;
+					idType = 0;
+					side = "East";
+					size = "Company";
+					type = "Maintenance";
+					commander = "RussianMen";
+					commanderRank = "Captain";
+					text = "%2 %3";
+					textShort = "%2";
+				};
+				class brservice
+				{
+					id = 7;
+					idType = 0;
+					side = "East";
+					size = "Company";
+					type = "Service";
+					commander = "RussianMen";
+					commanderRank = "Captain";
+					text = "%2 %3";
+					textShort = "%2";
+				};
+		};
+	};
 };
