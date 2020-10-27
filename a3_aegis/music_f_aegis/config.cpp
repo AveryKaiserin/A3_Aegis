6b4bcfd0e1da1f04e7b4bd386a6dffe0f36edb8f
Senicluxus <senicluxus@gmail.com>
2020-10-27 18:03:40 -0400

    Added 6 new tracks

--- a3_aegis/music_f_aegis/config.cpp
+++ a3_aegis/music_f_aegis/config.cpp
@@ -152,7 +152,7 @@ class CfgMusic
 	class AmbientTrack04b_F
 	{
 		name = $STR_A3_A_CfgMusic_AmbientTrack04b_F;
-		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\AmbientTrack04b_F.ogg",1,1};
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\AmbientTrack04b_F.ogg",db0,1};
 		duration = 218;
 		theme = stealth;
 		musicClass = Stealth;
@@ -160,64 +160,106 @@ class CfgMusic
 	class LeadTrack04_F_EPA
 	{
 		name = $STR_A3_A_CfgMusic_LeadTrack04_F_EPA;
-		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack04_F_EPA.ogg",1,1};
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack04_F_EPA.ogg",db0,1};
 		duration = 161;
 		musicClass = Calm;
 	};
 	class LeadTrack02a_F_EXP
 	{
 		name = $STR_A3_A_CfgMusic_LeadTrack02a_F_EXP;
-		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack02a_F_EXP.ogg",1,1};
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack02a_F_EXP.ogg",db0,1};
 		duration = 155;
 		musicClass = Action;
 	};
 	class LeadTrack05_F_EXP
 	{
 		name = $STR_A3_A_CfgMusic_LeadTrack05_F_EXP;
-		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack05_F_EXP.ogg",1,1};
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack05_F_EXP.ogg",db0,1};
 		duration = 60;
 		musicClass = Action;
 	};
 	class AmbientTrack03_F_EXP
 	{
 		name = $STR_A3_A_CfgMusic_AmbientTrack03_F_EXP;
-		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\AmbientTrack03_F_EXP.ogg",1,1};
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\AmbientTrack03_F_EXP.ogg",db0,1};
 		duration = 95;
 		musicClass = Calm;
 	};
 	class AmbientTrack04_F_EXP
 	{
 		name = $STR_A3_A_CfgMusic_AmbientTrack04_F_EXP;
-		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\AmbientTrack04_F_EXP.ogg",1,1};
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\AmbientTrack04_F_EXP.ogg",db0,1};
 		duration = 40;
 		musicClass = Calm;
 	};
 	class LeadTrack01a_F_Jets
 	{
 		name = $STR_A3_A_CfgMusic_LeadTrack01a_F_Jets;
-		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack01a_F_Jets.ogg",1,1};
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack01a_F_Jets.ogg",db0,1};
 		duration = 184;
 		musicClass = Lead;
 	};
 	class LeadTrack03_F_Jets
 	{
 		name = $STR_A3_A_CfgMusic_LeadTrack03_F_Jets;
-		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack03_F_Jets.ogg",1,1};
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack03_F_Jets.ogg",db0,1};
 		duration = 113;
 		musicClass = Lead;
 	};
 	class LeadTrack03_F_Malden
 	{
 		name = $STR_A3_A_CfgMusic_LeadTrack03_F_Malden;
-		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack03_F_Malden.ogg",1,1};
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack03_F_Malden.ogg",db0,1};
 		duration = 138;
 		musicClass = Lead;
 	};
 	class LeadTrack02_F_Orange
 	{
 		name = $STR_A3_A_CfgMusic_LeadTrack02_F_Orange;
-		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack02_F_Orange.ogg",1,1};
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack02_F_Orange.ogg",db0,1};
 		duration = 149;
 		musicClass = Calm;
+	};
+	class LeadTrack01_F_Aegis
+	{
+		name = $STR_A3_A_CfgMusic_LeadTrack01_F_Aegis;
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack01_F_Aegis.ogg",db0,1};
+		duration = 133;
+		musicClass = Lead;
+	};
+	class LeadTrack02_F_Aegis
+	{
+		name = $STR_A3_A_CfgMusic_LeadTrack02_F_Aegis;
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack02_F_Aegis.ogg",db0,1};
+		duration = 136;
+		musicClass = Lead;
+	};
+	class LeadTrack03_F_Aegis
+	{
+		name = $STR_A3_A_CfgMusic_LeadTrack03_F_Aegis;
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack03_F_Aegis.ogg",db0,1};
+		duration = 161;
+		musicClass = Lead;
+	};
+	class LeadTrack04_F_Aegis
+	{
+		name = $STR_A3_A_CfgMusic_LeadTrack04_F_Aegis;
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack04_F_Aegis.ogg",db0,1};
+		duration = 130;
+		musicClass = Lead;
+	};
+	class LeadTrack05_F_Aegis
+	{
+		name = $STR_A3_A_CfgMusic_LeadTrack05_F_Aegis;
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack05_F_Aegis.ogg",db0,1};
+		duration = 184;
+		musicClass = Lead;
+	};
+	class LeadTrack06_F_Aegis
+	{
+		name = $STR_A3_A_CfgMusic_LeadTrack06_F_Aegis;
+		sound[] = {"\A3_Aegis\Music_F_Aegis\Music\LeadTrack06_F_Aegis.ogg",db0,1};
+		duration = 186;
+		musicClass = Lead;
 	};
 };
