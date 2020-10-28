c689b35b654df1d4b66ecd8e8fe0cdc853bd877d
AveryTheKitty <nightmare.nkl@gmail.com>
2020-10-28 17:02:42 -0500

    Update cfgAmmo.hpp

--- a3_wip/weapons_f_flash/cfgAmmo.hpp
+++ a3_wip/weapons_f_flash/cfgAmmo.hpp
@@ -6,9 +6,10 @@ class CfgAmmo
 	class GrenadeFlash: GrenadeHand
 	{
         /*
-        - Impact grenade
-        - Slightly larger blast radius than the Frag Grenade
-        - Slightly heavier than the Frag Grenade
+        - Flashbang
+        - Does a slight amount of damage if you're right next to it
+        - 2.3 second delay, we'll go with 3 seconds to compensate for Arma's clunkiness
+        - We'll just use the mini grenade's explosion sounds
         */
 		hit = 2;
 		indirectHit = 2;
@@ -17,8 +18,11 @@ class CfgAmmo
 		suppressionRadiusHit = 12;
 		typicalspeed = 22;
 		model = "\A3_Aegis\Weapons_F_Aegis\Ammo\Handgrenade_east_throw.p3d"; // Placeholder
+		explosionEffectsRadius = 0.5;
 		deflecting = 10;
         explosionTime = 3;
 		timeToLive = 18;
+		CraterEffects = NoCrater;
+		explosionEffects = ExploAmmoExplosion; //TODO: add custom explosion effects
 	};
 };
