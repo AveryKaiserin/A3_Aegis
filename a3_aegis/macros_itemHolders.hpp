7c7922f49d2a484e9eaa7ccbb421574bac91033e
AveryTheKitty <nightmare.nkl@gmail.com>
2020-11-15 17:06:39 -0600

    Update macros_itemHolders.hpp

--- a3_aegis/macros_itemHolders.hpp
+++ a3_aegis/macros_itemHolders.hpp
@@ -1,95 +1,94 @@
-/*
-- Holders for weapons, items, et cetera
-- Placed in CfgVehicles
+/* Macros: Item Holders for 3DEN editor and Curator interface
+- Placed in "cfgVehicles.hpp"
 */
 
 // Primary weapons
-#define WEAPON_HOLDER(weapon,magazine,name,subcategory) \
-	class Weapon_##weapon: Weapon_Base_F \
+#define WEAPON_HOLDER(a,b,c,d) \
+	class Weapon_##a##: Weapon_Base_F \
 	{ \
 		scope = public; \
 		scopeCurator = public; \
-		displayName = name; \
+		displayName = ##c##; \
 		author = $STR_A3_A_AveryTheKitty; \
 		editorCategory = EdCat_Weapons; \
-		editorSubcategory = subcategory; \
+		editorSubcategory = ##d##; \
 		vehicleClass = WeaponsPrimary; \
 		class TransportWeapons \
 		{ \
-			weap_xx(##weapon##,1); \
+			weap_xx(##a##,1); \
 		}; \
 		class TransportMagazines \
 		{ \
-			mag_xx(##magazine##,1); \
+			mag_xx(##b##,1); \
 		}; \
 	};
 
 // Handguns
-#define PISTOL_HOLDER(weapon,magazine,name) \
-	class Weapon_##weapon: Pistol_Base_F \
+#define PISTOL_HOLDER(a,b,c) \
+	class Weapon_##a##: Pistol_Base_F \
 	{ \
 		scope = public; \
 		scopeCurator = public; \
-		displayName = name; \
+		displayName = ##c##; \
 		author = $STR_A3_A_AveryTheKitty; \
 		editorCategory = EdCat_Weapons; \
 		editorSubcategory = EdSubcat_Pistols; \
 		vehicleClass = WeaponsHandguns; \
 		class TransportWeapons \
 		{ \
-			weap_xx(##weapon##,1); \
+			weap_xx(##a##,1); \
 		}; \
 		class TransportMagazines \
 		{ \
-			mag_xx(##magazine##,1); \
+			mag_xx(##b##,1); \
 		}; \
 	};
 
 // Secondary weapons
-#define LAUNCHER_HOLDER(weapon,magazine,name) \
-	class Weapon_##weapon: Launcher_Base_F \
+#define LAUNCHER_HOLDER(a,b,c) \
+	class Weapon_##a##: Launcher_Base_F \
 	{ \
 		scope = public; \
 		scopeCurator = public; \
-		displayName = name; \
+		displayName = ##c##; \
 		author = $STR_A3_A_AveryTheKitty; \
 		editorCategory = EdCat_Weapons; \
 		editorSubcategory = EdSubcat_Launchers; \
 		vehicleClass = WeaponsSecondary; \
 		class TransportWeapons \
 		{ \
-			weap_xx(##weapon##,1); \
+			weap_xx(##a##,1); \
 		}; \
 		class TransportMagazines \
 		{ \
-			mag_xx(##magazine##,1); \
+			mag_xx(##b##,1); \
 		}; \
 	};
 
 // Attachments
-#define ACC_HOLDER(accessory,name,category,subcategory,vehclass) \
-	class Item_##accessory: Item_Base_F \
+#define ACC_HOLDER(a,b,c,d,e) \
+	class Item_##a##: Item_Base_F \
 	{ \
 		scope = public; \
 		scopeCurator = public; \
-		displayName = name; \
+		displayName = ##b##; \
 		author = $STR_A3_A_AveryTheKitty; \
-		editorCategory = category; \
-		editorSubcategory = subcategory; \
-		vehicleClass = vehclass; \
+		editorCategory = ##c##; \
+		editorSubcategory = ##d##; \
+		vehicleClass = ##e##; \
 		class TransportItems \
 		{ \
-			item_xx(##accessory##,1); \
+			item_xx(##a##,1); \
 		}; \
 	};
 
 // Inventory items
-#define ITEM_HOLDER(item,name) \
-	class Item_##item: Item_Base_F \
+#define ITEM_HOLDER(a,b) \
+	class Item_##a##: Item_Base_F \
 	{ \
 		scope = public; \
 		scopeCurator = public; \
-		displayName = name; \
+		displayName = ##b##; \
 		author = $STR_A3_A_AveryTheKitty; \
 		editorCategory = EdCat_Equipment; \
 		editorSubcategory = EdSubcat_InventoryItems; \
@@ -97,7 +96,7 @@
 		model = "\A3\Weapons_F\DummyItem.p3d"; \
 		class TransportItems \
 		{ \
-			item_xx(##item##,1); \
+			item_xx(##b##,1); \
 		}; \
 	};
 
