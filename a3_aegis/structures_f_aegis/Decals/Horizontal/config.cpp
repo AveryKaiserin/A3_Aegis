22a0204a067b0febbb193c8e818fdb9b6f8b94e5
Senicluxus <senicluxus@gmail.com>
2020-11-16 13:19:14 -0500

    Aegis - Structures: Fixed CfgVehicles

--- a3_aegis/structures_f_aegis/Decals/Horizontal/config.cpp
+++ a3_aegis/structures_f_aegis/Decals/Horizontal/config.cpp
@@ -1,15 +1,8 @@
-#include "\A3_Aegis\basicDefines_A3.hpp"
+/* Header */
 #include "cfgPatches.hpp"
-class CfgVehicles
-{
-	class Land_DirtPatch_02_base_F;
-	class Land_DirtPatch_06_F: Land_DirtPatch_02_base_F
-	{
-		author = $STR_A3_A_AveryTheKitty;
-		editorPreview = "\A3_Aegis\EditorPreviews_F_Aegis\Data\CfgVehicles\Land_DirtPatch_06_F.jpg";
-		scope = public;
-		scopeCurator = public;
-		displayName = $STR_A3_A_CfgVehicles_Land_DirtPatch_06_F0;
-		model = "\A3_Aegis\Structures_F_Aegis\Decals\Horizontal\DirtPatch_06_F.p3d";
-	};
-};
+
+/* Definitions & Inheritance */
+#include "\A3_Aegis\basicDefines_A3.hpp"
+
+/* Configuration */
+#include "cfgVehicles.hpp"
--- a3_aegis/structures_f_aegis/Humanitarian/Camps/config.cpp
+++ a3_aegis/structures_f_aegis/Humanitarian/Camps/config.cpp
@@ -1,45 +1,8 @@
-#include "\A3_Aegis\basicDefines_A3.hpp"
+/* Header */
 #include "cfgPatches.hpp"
-class CfgVehicles
-{
-    // Arma 3
-	class Camping_base_F;
-    // Arma 3 Laws of War
-    class Land_MedicalTent_01_base_F;
-	class Land_MedicalTent_01_white_IDAP_med_closed_F: Land_MedicalTent_01_base_F
-	{
-		editorCategory = EdCat_Structures;
-	};
-	class Land_MedicalTent_01_white_generic_closed_F: Land_MedicalTent_01_base_F
-	{
-		editorCategory = EdCat_Structures;
-	};
-	class Land_MedicalTent_01_white_generic_open_F: Land_MedicalTent_01_base_F
-	{
-		editorCategory = EdCat_Structures;
-	};
-	class Land_MedicalTent_01_white_generic_outer_F: Land_MedicalTent_01_base_F
-	{
-		editorCategory = EdCat_Structures;
-	};
-	class Land_MedicalTent_01_white_generic_inner_F: Land_MedicalTent_01_base_F
-	{
-		editorCategory = EdCat_Structures;
-	};
-	class Land_MedicalTent_01_white_IDAP_closed_F: Land_MedicalTent_01_base_F
-	{
-		editorCategory = EdCat_Structures;
-	};
-	class Land_MedicalTent_01_white_IDAP_open_F: Land_MedicalTent_01_base_F
-	{
-		editorCategory = EdCat_Structures;
-	};
-	class Land_MedicalTent_01_white_IDAP_outer_F: Land_MedicalTent_01_base_F
-	{
-		editorCategory = EdCat_Structures;
-	};
-	class Land_MedicalTent_01_floor_base_F: Camping_base_F
-	{
-		editorCategory = EdCat_Structures;
-	};
-};
+
+/* Definitions & Inheritance */
+#include "\A3_Aegis\basicDefines_A3.hpp"
+
+/* Configuration */
+#include "cfgVehicles.hpp"
