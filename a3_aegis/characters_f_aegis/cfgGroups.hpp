90544133a5e712cf116479fb2b7e8d56f0c923fd
Senicluxus <senicluxus@gmail.com>
2020-11-17 20:52:47 -0500

    Aegis - RUS Marid replaced with BTR-100

--- a3_aegis/characters_f_aegis/cfgGroups.hpp
+++ a3_aegis/characters_f_aegis/cfgGroups.hpp
@@ -2984,15 +2984,13 @@ class CfgGroups
 					side = TEast;
 					faction = OPF_R_F;
 					icon = "\A3\UI_F\Data\Map\Markers\NATO\o_mech_inf.paa";
-					GROUP_UNIT(0,TEast,O_R_APC_Wheeled_02_rcws_v2_F,LIEUTENANT,0,0);
+					GROUP_UNIT(0,TEast,O_R_APC_Wheeled_04_cannon_F,LIEUTENANT,0,0);
 					GROUP_UNIT(1,TEast,O_R_soldier_SL_F,SERGEANT,5,-5);
-					GROUP_UNIT(2,TEast,O_R_RadioOperator_F,PRIVATE,-5,-5);
-					GROUP_UNIT(3,TEast,O_R_soldier_LAT_F,CORPORAL,10,-10);
-					GROUP_UNIT(4,TEast,O_R_soldier_M_F,PRIVATE,-10,-10);
-					GROUP_UNIT(5,TEast,O_R_soldier_TL_F,SERGEANT,15,-15);
-					GROUP_UNIT(6,TEast,O_R_soldier_AR_F,CORPORAL,-15,-15);
-					GROUP_UNIT(7,TEast,O_R_soldier_A_F,PRIVATE,20,-20);
-					GROUP_UNIT(8,TEast,O_R_medic_F,PRIVATE,-20,-20);
+					GROUP_UNIT(2,TEast,O_R_soldier_LAT_F,CORPORAL,10,-10);
+					GROUP_UNIT(3,TEast,O_R_soldier_M_F,PRIVATE,-10,-10);
+					GROUP_UNIT(4,TEast,O_R_soldier_TL_F,SERGEANT,15,-15);
+					GROUP_UNIT(5,TEast,O_R_soldier_AR_F,CORPORAL,-15,-15);
+					GROUP_UNIT(6,TEast,O_R_medic_F,PRIVATE,-20,-20);
 				};
 				class O_R_MechInf_AT
 				{
@@ -3032,15 +3030,13 @@ class CfgGroups
 					side = TEast;
 					faction = OPF_R_F;
 					icon = "\A3\UI_F\Data\Map\Markers\NATO\o_mech_inf.paa";
-					GROUP_UNIT(0,TEast,O_R_APC_Wheeled_02_rcws_v2_F,LIEUTENANT,0,0);
+					GROUP_UNIT(0,TEast,O_R_APC_Wheeled_04_cannon_F,LIEUTENANT,0,0);
 					GROUP_UNIT(1,TEast,O_R_soldier_SL_F,SERGEANT,5,-5);
 					GROUP_UNIT(2,TEast,O_R_soldier_TL_F,SERGEANT,-5,-5);
 					GROUP_UNIT(3,TEast,O_R_soldier_repair_F,CORPORAL,10,-10);
 					GROUP_UNIT(4,TEast,O_R_engineer_F,PRIVATE,-10,-10);
 					GROUP_UNIT(5,TEast,O_R_medic_F,PRIVATE,15,-15);
-					GROUP_UNIT(6,TEast,O_R_soldier_AR_F,CORPORAL,-15,-15);
-					GROUP_UNIT(7,TEast,O_R_soldier_exp_F,PRIVATE,20,-20);
-					GROUP_UNIT(8,TEast,O_R_soldier_A_F,PRIVATE,-20,-20);
+					GROUP_UNIT(6,TEast,O_R_soldier_exp_F,PRIVATE,20,-20);
 				};
 			};
 			class Armored
@@ -3506,15 +3502,13 @@ class CfgGroups
 					side = TEast;
 					faction = OPF_R_ard_F;
 					icon = "\A3\UI_F\Data\Map\Markers\NATO\o_mech_inf.paa";
-					GROUP_UNIT(0,TEast,O_R_APC_Wheeled_02_rcws_v2_ard_F,LIEUTENANT,0,0);
+					GROUP_UNIT(0,TEast,O_R_APC_Wheeled_04_cannon_ard_F,LIEUTENANT,0,0);
 					GROUP_UNIT(1,TEast,O_R_soldier_SL_ard_F,SERGEANT,5,-5);
-					GROUP_UNIT(2,TEast,O_R_RadioOperator_ard_F,PRIVATE,-5,-5);
-					GROUP_UNIT(3,TEast,O_R_soldier_LAT_ard_F,CORPORAL,10,-10);
-					GROUP_UNIT(4,TEast,O_R_soldier_M_ard_F,PRIVATE,-10,-10);
-					GROUP_UNIT(5,TEast,O_R_soldier_TL_ard_F,SERGEANT,15,-15);
-					GROUP_UNIT(6,TEast,O_R_soldier_AR_ard_F,CORPORAL,-15,-15);
-					GROUP_UNIT(7,TEast,O_R_soldier_A_ard_F,PRIVATE,20,-20);
-					GROUP_UNIT(8,TEast,O_R_medic_ard_F,PRIVATE,-20,-20);
+					GROUP_UNIT(2,TEast,O_R_soldier_LAT_ard_F,CORPORAL,10,-10);
+					GROUP_UNIT(3,TEast,O_R_soldier_M_ard_F,PRIVATE,-10,-10);
+					GROUP_UNIT(4,TEast,O_R_soldier_TL_ard_F,SERGEANT,15,-15);
+					GROUP_UNIT(5,TEast,O_R_soldier_AR_ard_F,CORPORAL,-15,-15);
+					GROUP_UNIT(6,TEast,O_R_medic_ard_F,PRIVATE,-20,-20);
 				};
 				class O_R_MechInf_AT
 				{
@@ -3554,15 +3548,13 @@ class CfgGroups
 					side = TEast;
 					faction = OPF_R_ard_F;
 					icon = "\A3\UI_F\Data\Map\Markers\NATO\o_mech_inf.paa";
-					GROUP_UNIT(0,TEast,O_R_APC_Wheeled_02_rcws_v2_ard_F,LIEUTENANT,0,0);
+					GROUP_UNIT(0,TEast,O_R_APC_Wheeled_04_cannon_ard_F,LIEUTENANT,0,0);
 					GROUP_UNIT(1,TEast,O_R_soldier_SL_ard_F,SERGEANT,5,-5);
 					GROUP_UNIT(2,TEast,O_R_soldier_TL_ard_F,SERGEANT,-5,-5);
 					GROUP_UNIT(3,TEast,O_R_soldier_repair_ard_F,CORPORAL,10,-10);
 					GROUP_UNIT(4,TEast,O_R_engineer_ard_F,PRIVATE,-10,-10);
 					GROUP_UNIT(5,TEast,O_R_medic_ard_F,PRIVATE,15,-15);
-					GROUP_UNIT(6,TEast,O_R_soldier_AR_ard_F,CORPORAL,-15,-15);
-					GROUP_UNIT(7,TEast,O_R_soldier_exp_ard_F,PRIVATE,20,-20);
-					GROUP_UNIT(8,TEast,O_R_soldier_A_ard_F,PRIVATE,-20,-20);
+					GROUP_UNIT(6,TEast,O_R_soldier_exp_ard_F,PRIVATE,20,-20);
 				};
 			};
 			class Armored
