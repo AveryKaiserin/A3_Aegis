391288c3be72c41bb712821ece98c36d7261fde9
Senicluxus <senicluxus@gmail.com>
2021-02-25 12:37:25 -0500

    AEGIS WIP - Armored SUV
    Shadow bug - Windows not breaking bug - update some textures (rear light / license plate / dashboard)

---
+++ a3_wip/soft_f_wip/SUV_02/cfgWeapons.hpp
@@ -0,0 +1,8 @@
+class CfgWeapons
+{
+	class LMG_Minigun_Transport;
+    class DEGA_LMG_Minigun_Suv: LMG_Minigun_Transport
+	{
+		displayName = $STR_A3_A_CfgWeapons_LMG_Minigun0;
+	};
+};
---
+++ a3_wip/soft_f_wip/SUV_02/data/release/mod.cpp
@@ -0,0 +1,15 @@
+
+name = "DEGA: ArmA 2 PMC SUV Port v1.0";
+description = "DEGA: ArmA 2 PMC SUV Modification";
+picture = "\dega_vehicles_suv\data\release\logo_dega.paa";
+actionName = "";
+action = "";
+
+logo = "\dega_vehicles_suv\data\release\logo_dega.paa";
+logoOver = "\dega_vehicles_suv\data\release\logo_dega.paa";
+logoSmall = "\dega_vehicles_suv\data\release\logo_dega_small.paa";
+
+tooltip = "Deltagamer: PMC SUV Port";
+tooltipOwned = "Deltagamer: PMC SUV Port - Owned";
+overview = "PMC SUV Port: 2009 Era Chevy inspired Suburban with many Virtual Garage features";
+author = "Deltagamer";
