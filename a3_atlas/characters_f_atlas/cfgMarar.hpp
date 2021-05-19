45b029ba807f3614bc740da44ae6393a4f8100e3
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-05-19 02:29:47 -0700

    ATLAS - Haha Just kidding
    Infantry loadouts weren't finished. Protip, don't code at 2 in the fucking morning. Fixed bugs, you're welcome.

--- a3_atlas/characters_f_atlas/cfgMarar.hpp
+++ a3_atlas/characters_f_atlas/cfgMarar.hpp
@@ -351,7 +351,7 @@ class Atlas_B_M_Soldier_LAT_F: Atlas_B_M_Soldier_Base_F
     scope = public;
     scopeCurator = public;
 	displayName = $STR_B_Soldier_lat_F0;
-	backpack = B_AssaultPack_marar_medic;
+	backpack = B_AssaultPack_marar_lat;
 	linkedItems[] =
 	{
 		V_PlateCarrier2_snd,
@@ -368,15 +368,15 @@ class Atlas_B_M_Soldier_LAT_F: Atlas_B_M_Soldier_Base_F
 	};
 	weapons[] =
     {
-        arifle_mk20_plain_arco_pointer_F,
-        launch_MRAWS_sand_rail_F
+        arifle_mk20_plain_holo_pointer_F,
+        launch_MRAWS_sand_rail_F,
         hgun_G17_black_F,
         DefaultManWeapons
     };
     respawnWeapons[] =
     {
-        arifle_mk20_plain_arco_pointer_F,
-        launch_MRAWS_sand_rail_F
+        arifle_mk20_plain_holo_pointer_F,
+        launch_MRAWS_sand_rail_F,
         hgun_G17_black_F,
         DefaultManWeapons
     };
@@ -457,7 +457,7 @@ class Atlas_B_M_Helipilot_F: Atlas_B_M_Soldier_Base_F
     scope = public;
     scopeCurator = public;
 	displayName = $STR_B_Helipilot_F0;
-	uniformClass = Atlas_U_B_M_CombatUniform_des;
+	uniformClass = Atlas_U_B_M_Tank_Marar_F;
 	linkedItems[] =
 	{
 		V_TacVest_brn,
@@ -474,12 +474,12 @@ class Atlas_B_M_Helipilot_F: Atlas_B_M_Soldier_Base_F
 	};
 	weapons[] =
     {
-        SMG_04_F,
+        SMG_04_blk_F,
         DefaultManWeapons
     };
     respawnWeapons[] =
     {
-        SMG_04_F,
+        SMG_04_blk_F,
         DefaultManWeapons
     };
     magazines[] =
@@ -503,7 +503,7 @@ class Atlas_B_M_Helicrew_F: Atlas_B_M_Soldier_Base_F
     scope = public;
     scopeCurator = public;
 	displayName = $STR_B_helicrew_F0;
-	uniformClass = Atlas_U_B_M_CombatUniform_des;
+	uniformClass = Atlas_U_B_M_Tank_Marar_F;
 	linkedItems[] =
 	{
 		V_TacVest_brn,
@@ -515,6 +515,58 @@ class Atlas_B_M_Helicrew_F: Atlas_B_M_Soldier_Base_F
 	{
 		V_TacVest_brn,
 		H_CrewHelmetHeli_B,
+		DefaultManLinkedItems,
+		NVgoggles
+	};
+	weapons[] =
+    {
+        arifle_mk20_plain_holo_pointer_F,
+        hgun_G17_black_F,
+        DefaultManWeapons
+    };
+    respawnWeapons[] =
+    {
+        arifle_mk20_plain_holo_pointer_F,
+        hgun_G17_black_F,
+        DefaultManWeapons
+    };
+    magazines[] =
+    {
+		mag_4(30Rnd_556x45_Stanag_red),
+        mag_2(17Rnd_9x21_Mag),
+        HandGrenade_East,
+        SmokeShell
+    };
+    respawnMagazines[] =
+    {
+		mag_4(30Rnd_556x45_Stanag_red),
+        mag_2(17Rnd_9x21_Mag),
+        HandGrenade_East,
+        SmokeShell
+    };
+	camouflage = 2;
+	cost = 160000;
+	role = Crewman;
+};
+class Atlas_B_M_Crew_F: Atlas_B_M_Soldier_Base_F
+{
+	author = $STR_A3_A_BranFlakes;
+	//editorPreview = "\A3_Atlas\EditorPreviews_F_Atlas\Data\CfgVehicles\Atlas_O_VZ_Soldier_F.jpg";
+    scope = public;
+    scopeCurator = public;
+	displayName = $STR_B_crew_F0;
+	uniformClass = Atlas_U_B_M_Tank_Marar_F;
+	linkedItems[] =
+	{
+		V_TacVest_brn,
+		H_HelmetCrew_B,
+		DefaultManLinkedItems,
+		NVgoggles
+	};
+	respawnLinkedItems[] =
+	{
+		V_TacVest_brn,
+		H_HelmetCrew_B,
 		DefaultManLinkedItems,
 		NVgoggles
 	};
@@ -555,6 +607,7 @@ class Atlas_B_M_SL_F: Atlas_B_M_Soldier_Base_F
     scope = public;
     scopeCurator = public;
 	displayName = $STR_B_Soldier_SL_F0;
+	uniformClass = Atlas_U_B_M_CombatUniform_shortsleeve_des;
 	linkedItems[] =
 	{
 		V_PlateCarrier2_snd,
@@ -607,7 +660,7 @@ class Atlas_B_M_Officer_F: Atlas_B_M_Soldier_Base_F
 	//editorPreview = "\A3_Atlas\EditorPreviews_F_Atlas\Data\CfgVehicles\Atlas_O_VZ_Soldier_F.jpg";
     scope = public;
     scopeCurator = public;
-	displayName = $STR_A3_CfgVehicles_B_Soldier_F0;
+	displayName = $STR_B_officer_F0;
 	linkedItems[] =
 	{
 		V_Rangemaster_Belt_cbr,
@@ -654,6 +707,7 @@ class Atlas_B_M_RadioOperator_F: Atlas_B_M_Soldier_Base_F
     scope = public;
     scopeCurator = public;
     displayName = $STR_A3_C_B_W_RadioOperator_F0;
+    uniformClass = Atlas_U_B_M_CombatUniform_shortsleeve_des;
     backpack = B_RadioBag_01_marar_F;
     linkedItems[] =
     {
